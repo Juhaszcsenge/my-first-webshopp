cc5c5018b2ba279a425311909a3b932ee005e26f
Juhász Csenge <csenge799@gmail.com>
2023-02-02 22:59:22 +0100

    kosárhoz való hozzáadás és elvétel

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 import { formatCurrency } from '../utilities/formatCurrency';
 
 
@@ -12,10 +13,24 @@ type StoreItemProps = {
 
 export function StoreItem ({id, name, price, imgUrl}:
     StoreItemProps) {
-        return <Card>
+       
+        const {
+            getItemQuantity,
+            increaseCartQuantity,
+            decreaseCartQuantity,
+            removeFromCart,
+        }  = useShoppingCart()
+        
+        
+        const quantity = getItemQuantity(id)
+        
+        
+        return (
+            <Card className="h-100">
             <Card.Img
             variant="top"  
-            src={imgUrl} height="200px" 
+            src={imgUrl} 
+            height="200px" 
             style={{objectFit: "cover"}}
             />
             <Card.Body className="d-felx felx-column">
@@ -24,6 +39,38 @@ export function StoreItem ({id, name, price, imgUrl}:
                 <span className="fs-2">{name}</span>
                 <span className="ms-2 text-muted">{formatCurrency(price)}</span>
               </Card.Title>  
+              <div className="mt-auto">
+                {quantity === 0 ? (
+                    <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+                        + Add to Cart
+                    </Button>
+                ) : (
+                    <div 
+                    className="d-felx align-items-center flex-column"
+                    style={{ gap: ".5rem" }}
+                    >
+                        <div
+                        className="d-felx align-items-center jutofy-content-center"
+                        style={{gap: ".5rem" }}
+                        >
+                            <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                            <div>
+                                <span className="fs-3">{quantity}</span> in cart
+                            </div>
+                            <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+                        </div>
+                        <Button 
+                        onClick={() => removeFromCart(id)}
+                        variant="danger"
+                        size="sm"
+                        >
+                            Remove
+                        </Button>    
+                    </div>
+                )}
+
+              </div>
             </Card.Body>
         </Card>
+        )
     }
