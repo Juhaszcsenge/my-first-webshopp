4ac3c3f615f396685414535140a1be0bec9122a3
Juhász Csenge <cse4nge799@gmail.com>
2023-02-03 12:30:00 +0100

    boostrap végre sikeres importálása

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "bootstrap/dist/css/bootsrtap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
